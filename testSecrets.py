035eb7752ab8bcc1e0405e7833077f775b949311
toNyc2017 <olds.tom@gmail.com>
2024-08-16 21:56:39 -0400

    Create testSecrets.py

---
+++ testSecrets.py
@@ -0,0 +1,12 @@
+import os
+import hashlib
+
+# Retrieve the environment variable
+secret_value = os.getenv('AZURE_STORAGE_ACCOUNT_PASSWORD')
+
+if secret_value:
+    # Perform an operation using the secret
+    hashed_value = hashlib.sha256(secret_value.encode()).hexdigest()
+    print(f"Secret is set and its hash is: {hashed_value}")
+else:
+    print("Secret is not set.")
